1a95d949734a28df5c2cf43c6e945f97e0ee21c0
sql5731936 <kimi20244@hotmail.com>
2024-09-21 20:54:58 +0300

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,15 +1,4 @@
-<?php 
-
-/*
-
-
-SNLe4kUC83QAAWS
-
-/vxnXt4n!-8Uvg!
-
-
-
-عبدالكريم شريان مبخوت محمد عايض
+<?php
 
 
 
@@ -21,11 +10,11 @@ if(!defined('DB_SERVER')) define('DB_SERVER',"sql5.freemysqlhosting.net");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"sql5731936");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"sVSGefzKv3");
 if(!defined('DB_NAME')) define('DB_NAME',"sql5731936");
-*/
+
 ?>
 
 <?php
-
+/*
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
 if(!defined('base_url')) define('base_url','http://localhost/dms/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
@@ -35,5 +24,5 @@ if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
 if(!defined('DB_NAME')) define('DB_NAME',"sql5731936");
 
-
+*/
 ?>
